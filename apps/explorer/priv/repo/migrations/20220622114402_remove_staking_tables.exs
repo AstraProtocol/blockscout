59c21e0a1c03e42b3a4f4f104cc4f3d43c127e4d
Viktor Baranov <baranov.viktor.27@gmail.com>
2022-06-22 14:59:17 +0300

    Remove staking-related tables

--- apps/explorer/priv/repo/migrations/20220622114402_remove_staking_tables.exs
+++ apps/explorer/priv/repo/migrations/20220622114402_remove_staking_tables.exs
@@ -2,12 +2,7 @@ defmodule Explorer.Repo.Migrations.RemoveStakingTables do
   use Ecto.Migration
 
   def change do
-    execute("""
-    DROP TABLE staking_pools_delegators CASCADE;
-    """)
-
-    execute("""
-    DROP TABLE staking_pools CASCADE;
-    """)
+    drop_if_exists(table(:staking_pools))
+    drop_if_exists(table(:staking_pools_delegators))
   end
 end
