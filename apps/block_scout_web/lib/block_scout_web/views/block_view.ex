9f9f4fd70b477fba0ee770d3a90b90247758d1c6
Tien Nam Dao <tien.dao2@tiki.vn>
2022-07-19 16:45:53 +0700

    fix: build docker

--- apps/block_scout_web/lib/block_scout_web/views/block_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/block_view.ex
@@ -3,11 +3,10 @@ defmodule BlockScoutWeb.BlockView do
 
   import Math.Enum, only: [mean: 1]
 
-  alias Ecto.Association.NotLoaded
   alias Explorer.Chain
   alias Explorer.Chain.{Block, Wei}
   alias Explorer.Chain.Block.Reward
-  alias Explorer.Counters.BlockPriorityFeeCounter
+  alias Explorer.Counters.{BlockPriorityFeeCounter}
 
   @dialyzer :no_match
 
@@ -19,12 +18,11 @@ defmodule BlockScoutWeb.BlockView do
       |> Kernel.||(0)
       |> BlockScoutWeb.Cldr.Number.to_string!()
 
-    unit_text = gettext("NanoAstra")
+    unit_text = gettext("MicroAstra")
 
     "#{average} #{unit_text}"
   end
 
-  def block_type(%Block{consensus: false, nephews: %NotLoaded{}}), do: "Reorg"
   def block_type(%Block{consensus: false, nephews: []}), do: "Reorg"
   def block_type(%Block{consensus: false}), do: "Uncle"
   def block_type(_block), do: "Block"
@@ -60,12 +58,12 @@ defmodule BlockScoutWeb.BlockView do
       %{payout_key: block_miner_payout_address} = Reward.get_validator_payout_key_by_mining(block_miner_address)
 
       if beneficiary_address == block_miner_payout_address do
-        gettext("Proposer Reward")
+        gettext("Miner Reward")
       else
         gettext("Chore Reward")
       end
     else
-      gettext("Proposer Reward")
+      gettext("Miner Reward")
     end
   end
 
