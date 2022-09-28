b3f92d1e4ba505772240e16546ff090edbeaa83e
trung <trung.nguyen25@tiki.vn>
2022-09-28 10:55:47 +0700

    refactor: add rate limit for history chart apis

--- apps/block_scout_web/lib/block_scout_web/controllers/chain/market_history_chart_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/chain/market_history_chart_controller.ex
@@ -5,23 +5,27 @@ defmodule BlockScoutWeb.Chain.MarketHistoryChartController do
   alias Explorer.ExchangeRates.Token
 
   def show(conn, _params) do
-    exchange_rate = Market.get_exchange_rate(Explorer.coin()) || Token.null()
+    if ajax?(conn) do
+      exchange_rate = Market.get_exchange_rate(Explorer.coin()) || Token.null()
 
-    recent_market_history = Market.fetch_recent_history()
+      recent_market_history = Market.fetch_recent_history()
 
-    market_history_data =
-      case recent_market_history do
-        [today | the_rest] ->
-          encode_market_history_data([%{today | closing_price: exchange_rate.usd_value} | the_rest])
+      market_history_data =
+        case recent_market_history do
+          [today | the_rest] ->
+            encode_market_history_data([%{today | closing_price: exchange_rate.usd_value} | the_rest])
 
-        data ->
-          encode_market_history_data(data)
-      end
+          data ->
+            encode_market_history_data(data)
+        end
 
-    json(conn, %{
-      history_data: market_history_data,
-      supply_data: available_supply(Chain.supply_for_days(), exchange_rate)
-    })
+      json(conn, %{
+        history_data: market_history_data,
+        supply_data: available_supply(Chain.supply_for_days(), exchange_rate)
+      })
+    else
+      unprocessable_entity(conn)
+    end
   end
 
   defp available_supply(:ok, exchange_rate) do
