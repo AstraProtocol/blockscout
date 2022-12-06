ba68307d0c1813d5ab086be7be7bad2806a82d46
trung <trung.nguyen25@tiki.vn>
2022-12-06 16:33:40 +0700

    refactor: check address not found

--- apps/block_scout_web/lib/block_scout_web/controllers/api/rpc/address_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/api/rpc/address_controller.ex
@@ -27,10 +27,14 @@ defmodule BlockScoutWeb.API.RPC.AddressController do
   def getaddress(conn, params) do
     with {:address_param, {:ok, address_param}} <- fetch_address(params),
          {:format, {:ok, address_hash}} <- to_address_hash(address_param) do
-      {:ok, address} = Chain.hash_to_address(address_hash)
-      render(conn, "getaddress.json",
-                   %{address_detail: address, verified: Chain.smart_contract_fully_verified?(address_param)}
-            )
+      case Chain.hash_to_address(address_hash) do
+        {:ok, address} ->
+          render(conn, "getaddress.json",
+            %{address_detail: address, verified: Chain.smart_contract_fully_verified?(address_param)}
+          )
+        _ ->
+          render(conn, :error, error: "Address not found")
+      end
     else
       {:address_param, :error} ->
         render(conn, :error, error: "Query parameter 'address' is required")
